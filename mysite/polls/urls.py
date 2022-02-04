1b881019a9bc686fd6418bb1354564784a6c9f2e
ABC <sethumad@ualberta.ca>
2022-02-04 00:40:19 -0700

    namespacing url names

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
